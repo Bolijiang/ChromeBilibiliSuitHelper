b6ee822a094aae7b0d3ebb70aaf9a00b1c1472a8
afuet <73286581+lllk140@users.noreply.github.com>
2023-02-16 15:59:19 +0800

    Add files via upload

--- action/js/sort.js
+++ action/js/sort.js
@@ -43,6 +43,7 @@
                 return parseInt(a[key]) - parseInt(b[key]);
             });
             SetFanCards2Page(items);
+            updateTopButton();
         }
     }
 })();
@@ -99,6 +100,7 @@ document.getElementById("reverse-button").onclick = function() {
     const items = GetFanCardsItems();
     items.reverse();
     SetFanCards2Page(items);
+    updateTopButton();
 }
 
 document.getElementById("refresh-button").onclick = async function() {
@@ -122,87 +124,21 @@ document.getElementById("refresh-button").onclick = async function() {
     }
 }
 
-// document.getElementById("sort-option").onchange = async function() {
-//     const index = this.options.selectedIndex;
-// 	const value = this.options[index].value;
-//     if (!value) {
-//         return
-//     };
-
-//     const fanCardsList = document.getElementById(FanCardsList_Id);
-//     const fanCards = fanCardsList.getElementsByTagName("li");
-//     var cards = new Array();
-//     for (let i = 0; i < fanCards.length; i++) {
-//         cards[i] = fanCards[i]
-//     };
-//     if (value == "reverse") {
-//         cards.reverse();
-//     } else {
-//         cards.sort(function (a, b) {
-//             const aItme = parseFanCard(a);
-//             const bItme = parseFanCard(b);
-//             return parseInt(aItme[value]) - parseInt(bItme[value]);
-//         });
-//     };
-//     for (let i = 0; i < fanCards.length; i++) {
-//         const draging = cards[i];
-//         const target = fanCards[i];
-//         var targetRect = target.getBoundingClientRect();
-//         var dragingRect = draging.getBoundingClientRect();
-//         if (getIndex(draging) < getIndex(target)) {
-//             target.parentNode.insertBefore(draging, target.nextSibling);
-//         } else {
-//             target.parentNode.insertBefore(draging, target);
-//         };
-//         _animate(dragingRect, draging);
-//         _animate(targetRect, target);
-//     };
-// };
-
-
-// document.getElementById("fan-cards-sort").onclick = async function() {
-//     const root = document.getElementById(FanCardsList_Id);
-//     const fanCardsList = root.childNodes;
-
-//     var ids = new Array();
-//     for (let i = 0; i < fanCardsList.length; i++) {
-//         const item = parseFanCard(fanCardsList[i]);
-//         ids[i] = item["item_id"];
-//     };
-
-//     const res = await contentPage("ApplyMyFanCardsSort", {ids: ids});
-//     if (res["code"] != 0) {
-//         alert(res["message"]);
-//         return
-//     } else {
-//         alert("应用排序成功");
-//     };
-
-//     if (res["code"] == 0 && FanCardsListSaveLoacl) {
-//         console.log("保存数据到本地");
-//         await SaveItems2Local(null, null);
-//     };
-// };
-
-// document.getElementById("update-fan-cards").onclick = async function() {
-//     const user = await GetFanCardsTotal();
-
-//     if (user.code != 0) {
-//         console.log(user["message"]);
-//         return 
-//     };
-
-//     console.log("手动更新, 从网络更新到页面");
-//     const items = await GetFanCardsList(user.total);
-//     SetFardCards2Page(items);
-//     UpdateFanCardOnDbClick(TopAnimate);
-
-//     if (FanCardsListSaveLoacl) {
-//         console.log("保存数据到本地");
-//         await SaveItems2Local(null, null);
-//     };
-// };
-
-// (async function() {
-//     await StartLoadFanCards(TopAnimate, true);
-// })();
+document.getElementById("fan-cards-sort").onclick = async function() {
+    const ids = [];
+    const items = GetFanCardsItems();
+    for (let i = 0; i < items.length; i++) {
+        ids[i] = items[i]["item_id"].toString();
+    }
+    const res = await contentPage("ApplyMyFanCardsSort", {ids: ids});
+    if (res["code"] !== 0) {
+        alert(res["message"]);
+        return null;
+    } else {
+        alert("应用排序成功");
+    }
+    if (res["code"] === 0 && FanCardsListSaveLocal) {
+        console.log("保存数据到本地");
+        await SaveItems2Local(null, null);
+    }
+}
